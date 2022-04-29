e676c991be6b6727ef9c1adae5152027c393102b
Dawit <dawithayl1994@gmail.com>
2022-04-29 15:47:15 +0300

    Refined models and admin site configured

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
 
 # Create your views here.
 
 def HomePageView(request):
-    return HttpResponse("<h1>This is homepage for polls app!</h1>")
+    return HttpResponse(f"<h1>This is homepage for polls app!</h1>")
