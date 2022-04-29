4665a4e463cb1db2fe2b80111658726fcd595863
Dawit <dawithayl1994@gmail.com>
2022-04-29 18:09:40 +0300

    Added views and templates

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import HomePageView
+from .views import homepage,detail,results,vote
 
 urlpatterns = [
-	path('', HomePageView, name="home")
+	path('', homepage, name="home"),
+	path('polls/', homepage, name="home"),
+	path('polls/<int:question_id>', detail, name="detail"),
+	path('polls/<int:question_id>/results/', results, name="results"),
+	path('polls/<int:question_id>/vote/', vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponse
+from .models import Question as Q
 
 # Create your views here.
 
-def HomePageView(request):
-    return HttpResponse(f"<h1>This is homepage for polls app!</h1>")
+def homepage(request):
+	questions = Q.objects.order_by("-pub_date")[:5]
+	context = {'questions':questions}
+	return render(request, "polls/home.html", context)
+
+def detail(request, question_id):
+	"""
+		try:
+			question = Q.objects.get(id=question_id)
+		except Q.DoesNotExist:
+			raise Http404("<h1>Question Not Found !!!</h1>")
+	"""
+	question = get_object_or_404(Q, id=question_id)
+	return render(request, "polls/detail.html", {'question':question})
+
+def results(request, question_id):
+	return HttpResponse(f"<h2>You're looking at the results of {question_id}</h2>")
+
+def vote(request, question_id):
+	return HttpResponse(f"<h2>You're voting for the question {question_id}!</h2>")
